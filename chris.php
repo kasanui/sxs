384c4e604b8a3a87d21b078b532abca69b9f2de7
Chris Chen <chris@obvioussoftware.co.nz>
2014-01-19 16:52:51 +1300

    Got animation working

--- chris.php
+++ chris.php
@@ -15,8 +15,10 @@ while (!feof($file_handle) ) {
 
 $line_of_text = fgetcsv($file_handle, 1024);
 
-print $line_of_text[0] . $line_of_text[1]. $line_of_text[2] . "<BR>";
+print $line_of_text[0] + "<BR>";
+print $line_of_text[1];
 
+)
 }
 
 fclose($file_handle);
